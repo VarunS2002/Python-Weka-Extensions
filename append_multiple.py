0cd898889695217ea5fd35503ea1a82fdbb4a201
VarunS2002 <30069380+VarunS2002@users.noreply.github.com>
2021-08-13 16:29:25 +0530

    Added support for keeping program and datasets in different directories

--- append_multiple.py
+++ append_multiple.py
@@ -8,29 +8,55 @@ def id_generator(size: int = 10) -> str:
     return ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(size))
 
 
+print('Enter the installation directory of Weka')
+print('Example: C:\\Program Files\\Weka-3-8-5')
+print('Leave empty and press Enter if the program is in the installation directory')
+installation_dir: str = input('Path: ').strip()
+if not installation_dir == '':
+    installation_dir = installation_dir.replace('\\', '/').rstrip('\\').rstrip('/') + '/'
+
+print('Enter the path to directory containing all .arff files')
+print('Example: D:\\Users\\VarunS2002\\Desktop\\Weka Datasets\\samples')
+print('Leave empty and press Enter if the files are in the same directory as the program')
+files_dir: str = input('Path: ').strip()
+if not files_dir == '':
+    files_dir = files_dir.replace('\\', '/').rstrip('\\').rstrip('/') + '/'
+
+print('Enter the path for the output .arff file')
+print('Example: D:\\Users\\VarunS2002\\Desktop\\Weka Datasets\\samples\\output')
+print('Leave empty and press Enter if you want it to be in the same directory as the datasets')
+output_dir: str = input('Path: ').strip()
+if not output_dir == '':
+    output_dir = output_dir.replace('\\', '/').rstrip('\\').rstrip('/') + '/'
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+else:
+    output_dir = files_dir
+
 sys.argv.pop(0)
 if len(sys.argv) < 3:
     raise ValueError('Cannot append less than 2 files.\n'
                      'Syntax: python append_multiple.py '
                      'file1.arff file2.arff file3.arff '
                      'outputfile.arff')
-files: list[str] = sys.argv
-temporary_files: list[str] = [
-    id_generator() + '.arff' for _ in range(len(files) - 2)]
+files: list[str] = [files_dir + file_name for file_name in sys.argv]
+if files_dir != output_dir:
+    files[-1] = files[-1].replace(files_dir, output_dir)
+temporary_files: list[str] = [files_dir + id_generator() + '.arff' for _ in range(len(files) - 2)]
+
 for i in range(len(temporary_files)):
     if i == 0 and len(files) == 3:
-        os.system(f'java -classpath weka.jar weka.core.Instances append '
-                  f'{files[0]} {files[1]} > {files[2]}')
+        os.system(f'java -classpath "{installation_dir}weka.jar" weka.core.Instances append '
+                  f'"{files[0]}" "{files[1]}" > "{files[2]}"')
     elif i == 0:
-        os.system(f'java -classpath weka.jar weka.core.Instances append '
-                  f'{files[0]} {files[1]} > {temporary_files[i]}')
+        os.system(f'java -classpath "{installation_dir}weka.jar" weka.core.Instances append '
+                  f'"{files[0]}" "{files[1]}" > "{temporary_files[i]}"')
     elif i == len(temporary_files) - 1:
-        os.system(f'java -classpath weka.jar weka.core.Instances append '
-                  f'{temporary_files[i - 1]} {files[i + 1]} > {files[-1]}')
+        os.system(f'java -classpath "{installation_dir}weka.jar" weka.core.Instances append '
+                  f'"{temporary_files[i - 1]}" "{files[i + 1]}" > "{files[-1]}"')
         os.remove(temporary_files[i - 1])
     else:
-        os.system(f'java -classpath weka.jar weka.core.Instances append '
-                  f'{temporary_files[i - 1]} {files[i + 1]} > '
-                  f'{temporary_files[i]}')
+        os.system(f'java -classpath "{installation_dir}weka.jar" weka.core.Instances append '
+                  f'"{temporary_files[i - 1]}" "{files[i + 1]}" > '
+                  f'"{temporary_files[i]}"')
         os.remove(temporary_files[i - 1])
-
